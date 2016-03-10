7319930b6de05a85b2855349321e413016028a58
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-09 20:09:31 -0800

    add routes

--- samantha/routes/studentRoutes.js
+++ samantha/routes/studentRoutes.js
@@ -2,3 +2,9 @@
 
 var Router = require(__dirname + '/../lib/router')
 var http = require('http');
+var studentRouter = new Router();
+
+studentRouter.get('/students', (req, res) => {
+  console.log('/students route was hit')
+  res.end();
+})
